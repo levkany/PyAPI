d8d5b12370490a8e281962c0dd41d1dd2c82a9c5
levkany <levkany.dev@gmail.com>
2021-08-05 21:50:49 +0300

    updated endpoint examples & their documentation

--- endpoints/clients/clients.py
+++ endpoints/clients/clients.py
@@ -1,17 +1,20 @@
 from PyAPI.class_http_responder import HttpResponder
 
-def GET(data):
-        # Do something and return
-        return HttpResponder.build(507, {"whichEndPoint": "endpoint of GET"}, {'Content-type': 'application/json'})
+#
+# recieved params:
+# client:str - this is the client ip which initiated the request
+# method:str - this is request method, aka: GET/POST..
+# path:str   - this is the request resource path
+# body:str   - this is the request query string or Body data (depending on the request method) 
+#
+def callback(data):
+        #
+        # Example data:
+        # {'client': '127.0.0.1', 'method': 'GET', 'path': '/clients', 'body': 'hi=hello%20world'}
+        #
 
-def POST(data):
-        # Do something and return
-        return HttpResponder.build(200, 'endpoint of POST')
-
-def DELETE(data):
-        # Do something and return
-        return HttpResponder.build(200, 'endpoint of DELETE')
-
-def PUT(data):
-        # Do something and return
-        return HttpResponder.build(200, 'endpoint of PUT')
+        # The responder accepts the following aguments:
+        #  status_code:int = 200                - the status code to responde with
+        #  data:str|dict = empty dict           - the data to send to back to the request initiator
+        #  overrides:dict = empty dict          - overrides the responde headers
+        return HttpResponder.build(200, 'New clients callback', {'Content-type': 'text/html'})
--- endpoints/clients/clients_add.py
+++ endpoints/clients/clients_add.py
@@ -1,18 +1,20 @@
 from PyAPI.class_http_responder import HttpResponder
 
+#
+# recieved params:
+# client:str - this is the client ip which initiated the request
+# method:str - this is request method, aka: GET/POST..
+# path:str   - this is the request resource path
+# body:str   - this is the request query string or Body data (depending on the request method) 
+#
+def callback(data):
+        #
+        # Example data:
+        # {'client': '127.0.0.1', 'method': 'GET', 'path': '/clients/add', 'body': 'hi=hello%20world'}
+        #
 
-def GET(data):
-        # Do something and return
-        return HttpResponder.build(200, '/clients/add endpoint of GET')
-
-def POST(data):
-        # Do something and return
-        return HttpResponder.build(200, '/clients/add endpoint of POST')
-
-def DELETE(data):
-        # Do something and return
-        return HttpResponder.build(200, '/clients/add endpoint of DELETE')
-
-def PUT(data):
-        # Do something and return
-        return HttpResponder.build(200, '/clients/add endpoint of PUT')
+        # The responder accepts the following aguments:
+        #  status_code:int = 200                - the status code to responde with
+        #  data:str|dict = empty dict           - the data to send to back to the request initiator
+        #  overrides:dict = empty dict          - overrides the responde headers
+        return HttpResponder.build(200, 'New clients/add callback', {'Content-type': 'text/html'})

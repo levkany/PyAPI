041d481434ff4d05d4668e3622d30d4243e72c13
levkany <levkany.dev@gmail.com>
2021-08-21 22:17:41 +0300

    updated to reflect on recent changes

--- entry.py
+++ entry.py
@@ -2,26 +2,43 @@ from PyAPI.class_server import RestServer
 from PyAPI.class_endpoints import RestEndPoints
 from PyAPI.class_http_responder import HttpResponder
 
+
 # import endpoint callbacks
 from endpoints.clients import clients, clients_add
+
 
 # override 404 not found callback
 def __not_found_callback():
     return HttpResponder.build(404, '<h1>This API endpoint is not found!</h1>', {'Content-type': 'text/html'})
 
+
 # initialize endpoints handler
 endpoints = RestEndPoints(not_found_callback=__not_found_callback)
+
 
 def default_endpoint(data):
     welcome_data = open('welcome.html', 'r').readlines()
     data_to_send = ''.join(welcome_data)
     return HttpResponder.build(200, data_to_send, {'Content-type': 'text/html'})
 
+
 # add some endpoints
 endpoints.add('/', default_endpoint)
 endpoints.add('/clients', clients.callback)
 endpoints.add('/clients/add', clients_add.callback)
 
-# start the server
-server = RestServer('127.0.0.1', endpoints=endpoints, env='local')
+
+# initiaze the server
+server = RestServer('127.0.0.1', endpoints=endpoints, env='prod')
+
+# set maximum of allowed pending connections
+server.set_backlog(0)
+
+# load blacklist and add a few more ip's to block
+server.load_blacklist('./blacklist.txt')
+server.deny('127.0.0.2')
+server.deny('8.8.8.8')
+server.deny('11.11.11.11')
+
+# finally, we can start the server :)
 server.start()
--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -1,6 +1,36 @@
 import requests
-x = requests.delete('http://192.168.1.15:8820?hello"=world!', data={'param1': 'delete = this and that', 'param22': 'remove at all and ALL!! !! !'})
-x = requests.post('http://192.168.1.15:8820/clients/', data={'param1': 'delete = this and that', 'param22': 'remove at all and ALL!! !! !'})
-x = requests.get('http://192.168.1.15:8820/clients/add?hello=world')
-x = requests.get('http://192.168.1.15:8820/clients/notfoundcallback?hello=world')
-x = requests.get('http://192.168.1.15:8820/clients?redirect=123123')
+import random
+
+#
+#   Endpoint tests
+#
+
+# x = requests.delete('http://192.168.1.15:8820?hello"=world!', data={'param1': 'delete = this and that', 'param22': 'remove at all and ALL!! !! !'})
+# x = requests.post('http://192.168.1.15:8820/clients/', data={'param1': 'delete = this and that', 'param22': 'remove at all and ALL!! !! !'})
+# x = requests.get('http://192.168.1.15:8820/clients/add?hello=world')
+# x = requests.get('http://192.168.1.15:8820/clients/notfoundcallback?hello=world')
+# x = requests.get('http://192.168.1.15:8820/clients?redirect=123123')
+
+
+#
+#   Pending connection tests
+#
+
+# for x in range(100):
+#     data = requests.get('http://localhost:8820/' + str(random.random()))
+#     print(data)
+
+
+#
+#   File upload test
+#
+
+# files = {'upload_file': open('./test.pdf','rb'), 'upload_file_2': open('./file_2.txt','rb')}
+# x = requests.post('http://localhost:8820/', files=files, data={'username': 'hello world user', 'password': 'hello world pass!'})
+# print(x)
+
+
+# str1 = ''
+
+# for index, item in enumerate(str1.split('\r\n')):
+#     print('* ' + str(index) + ' - ' + item)
